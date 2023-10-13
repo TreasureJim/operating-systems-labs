cb423b822a99e8ed569b43fbf139af2a9f183b29
Liam Boreback <boreback.work@gmail.com>
2023-10-13 15:46:03 +0200

    Modify to allow for input of the addresses file path

--- sem3/optimal-page-fault-algorithm/src/main.rs
+++ sem3/optimal-page-fault-algorithm/src/main.rs
@@ -1,9 +1,12 @@
-const ADDRESS_FILE_PATH: &str = "../resources/addresses.txt";
+use std::env;
 
 mod memorymanager;
 
 fn main() {
-    let addresses_str = std::fs::read_to_string(ADDRESS_FILE_PATH).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let address_str = args.get(1).expect("No path for addresses provided");
+
+    let addresses_str = std::fs::read_to_string(address_str).unwrap();
     let addresses: Vec<u32> = addresses_str
         .lines()
         .map(|x| x.parse::<u32>().unwrap())
