e45fcdc36472e4abe6d0839d55c1ff94b13c4ae2
Liam Boreback <boreback.work@gmail.com>
2023-10-13 02:37:41 +0200

    Trying to calculate last used page - INCOMPLETE

--- sem3/rs-version/src/main.rs
+++ sem3/rs-version/src/main.rs
@@ -1,23 +1,13 @@
 const ADDRESS_FILE_PATH: &str = "../resources/addresses.txt";
-const PAGE_SIZE: usize = 256;
+
+mod memorymanager;
 
 fn main() {
     let addresses_str = std::fs::read_to_string(ADDRESS_FILE_PATH).unwrap();
-    let addresses: Vec<&str> = addresses_str.lines().collect();
+    let addresses: Vec<u32> = addresses_str
+        .lines()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
 
-    let manager = MemoryManager::new();
-}
-
-trait MemoryManager {}
-
-struct MemoryManager {
-    page_table: [Option<u16>; PAGE_SIZE],
-}
-
-impl MemoryManager {
-    pub fn new() -> Self {
-        Self {
-            page_table: [None; PAGE_SIZE],
-        }
-    }
+    let manager = memorymanager::MemoryManager::new(addresses);
 }
